9d216197ede16f6b806b273d58e3ebffb5c0bf64
smbifall <oleksmurke@gmail.com>
2024-07-24 14:12:00 +0300

    Initial commit

---
+++ src/tests/epam.test.js
@@ -0,0 +1,18 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe('Epam', () => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.epam.com/');
+  });
+
+  test('should have appropriate title', async ({ page }) => {
+    await expect(page).toHaveTitle('EPAM | Software Engineering & Product Development Services  ')
+  });
+
+  test('should open hamburger menu', async ({ page }) => {
+    await page.locator('button.hamburger-menu__button').click();
+    await expect(page.locator('div.hamburger-menu__dropdown-section')).toBeVisible();
+  });
+
+})
